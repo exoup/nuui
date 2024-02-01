691801da32f931170a70bee1c7b721d8297ef6f9
Joseph Young <josephy@dram.space>
2024-01-31 22:53:26 -0600

    feat: Add darkmode

--- .ladle/components.jsx
+++ .ladle/components.jsx
@@ -1,1 +1,13 @@
-import "tailwindcss/tailwind.css"
+import React, { useEffect } from "react";
+import "tailwindcss/tailwind.css";
+
+export const Provider = ({ children, globalState }) => {
+    useEffect(() => {
+        if (globalState.theme === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [globalState.theme]);
+    return <div className="p-4">{children}</div>;
+};
