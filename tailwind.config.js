db5460eb925fed41f1527cb7187742b4c2e7bf9d
Joseph Young <josephy@dram.space>
2024-02-01 22:06:49 -0600

    feat: Add some custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: 'class',
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                body: '#F8F9FC',
+                primary: '#4E73DF',
+                secondary: '#858796',
+                tertiary: '#8165A8',
+                success: '#1CC88A',
+                info: '#36B9CC',
+                warning: '#F6C23E',
+                danger: '#E74A3B',
+                light: '#F8F9FC',
+                dark: '#5A5C69'
+            },
+        },
     },
     variants: {},
     plugins: [require('@tailwindcss/forms')],
