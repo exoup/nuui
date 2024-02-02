3f60d5fc2c60719621f406c02c562ec078d81405
Joseph Young <josephy@dram.space>
2024-02-01 23:47:36 -0600

    feat: classOptions util for dynamic class loading

---
+++ src/util/classOptions.js
@@ -0,0 +1,29 @@
+export const colorOptions = {
+    body: 'bg-body',
+    primary: 'bg-primary',
+    secondary: 'bg-secondary',
+    tertiary: 'bg-tertiary',
+    grape: 'bg-grape',
+    info: 'bg-info',
+    success: 'bg-success',
+    warning: 'bg-warning',
+    danger: 'bg-danger',
+    light: 'bg-light text-dark',
+    dark: 'bg-dark'
+}
+
+export const radiusOptions = {
+    round: 'rounded-md',
+    sharp: 'rounded-none',
+    full: 'rounded-full'
+}
+
+export const sizeOptions = {
+    responsive: '',
+    defined: '',
+    full: ''
+}
+
+export const loadingOptions = {
+    "three-quarter": 'inline-block after:content-[" "] after:block after:size-6 after:border-inherit after:border-b-transparent after:border-[4px] after:rounded-[50%] after:border-solid after:animate-spin'
+}
