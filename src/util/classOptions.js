a9f691e607d1ae5c6a3b5b51fa0aaa4093d42783
Joseph Young <josephy@dram.space>
2024-02-03 00:44:33 -0600

    feat: Update button color styling

--- src/util/classOptions.js
+++ src/util/classOptions.js
@@ -1,4 +1,4 @@
-export const colorOptions = {
+export const buttonColorOptions = {
     primary: 'bg-primary text-white hover:bg-primary/80 hover:focus:bg-primary/70',
     secondary: 'bg-secondary text-white hover:bg-secondary/80 hover:focus:bg-secondary/70',
     tertiary: 'bg-tertiary text-white hover:bg-tertiary/80 hover:focus:bg-tertiary/70',
@@ -7,7 +7,7 @@ export const colorOptions = {
     success: 'bg-success text-white hover:bg-success/80 hover:focus:bg-success/70',
     warning: 'bg-warning text-white hover:bg-warning/80 hover:focus:bg-warning/70',
     danger: 'bg-danger text-white hover:bg-danger/80 hover:focus:bg-danger/70',
-    light: 'bg-light text-dark hover:bg-light/80 hover:focus:bg-light/70',
+    light: 'bg-light text-black hover:bg-gray-100 hover:focus:bg-light/70 ring-1 ring-inset ring-dark dark:bg-dark dark:text-light dark:hover:bg-dark/80 dark:hover:focus:bg-dark/70 dark:ring-info',
     dark: 'bg-dark text-white hover:bg-dark/80 hover:focus:bg-dark/70'
 };
 
@@ -19,12 +19,12 @@ export const radiusOptions = {
 
 export const sizeOptions = {
     responsive: 'flex-1 flex-shrink-0',
-    normal: 'min-w-24 max-w-28',
-    wide: 'min-w-80 max-w-96',
+    normal: 'min-w-24',
+    wide: 'min-w-80',
     full: 'w-full min-w-full'
 };
 
 export const loadingOptions = {
     "three-quarter": 'inline-block after:content-[" "] after:block after:size-6 after:border-white after:border-b-transparent after:border-[4px] after:rounded-[50%] after:border-solid after:animate-spin',
-    "three-quarter-dark": 'inline-block after:content-[" "] after:block after:size-6 after:border-dark after:border-b-transparent after:border-[4px] after:rounded-[50%] after:border-solid after:animate-spin'
+    "three-quarter-dark": 'inline-block after:content-[" "] after:block after:size-6 after:border-dark dark:after:border-white after:border-b-transparent dark:after:border-b-transparent after:border-[4px] after:rounded-[50%] after:border-solid after:animate-spin'
 };
