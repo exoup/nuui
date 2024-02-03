6fe27fa9d04874cd6e4970d006c7764da95aaa89
Joseph Young <josephy@dram.space>
2024-02-02 22:58:56 -0600

    feat: Update color options

--- src/util/classOptions.js
+++ src/util/classOptions.js
@@ -1,29 +1,30 @@
 export const colorOptions = {
-    body: 'bg-body',
-    primary: 'bg-primary',
-    secondary: 'bg-secondary',
-    tertiary: 'bg-tertiary',
-    grape: 'bg-grape',
-    info: 'bg-info',
-    success: 'bg-success',
-    warning: 'bg-warning',
-    danger: 'bg-danger',
-    light: 'bg-light text-dark',
-    dark: 'bg-dark'
-}
+    primary: 'bg-primary text-white hover:bg-primary/80 focus:bg-primary/70',
+    secondary: 'bg-secondary text-white hover:bg-secondary/80 focus:bg-secondary/70',
+    tertiary: 'bg-tertiary text-white hover:bg-tertiary/80 focus:bg-tertiary/70',
+    grape: 'bg-grape text-white hover:bg-grape/80 focus:bg-grape/70',
+    info: 'bg-info text-white hover:bg-info/80 focus:bg-info/70',
+    success: 'bg-success text-white hover:bg-success/80 focus:bg-success/70',
+    warning: 'bg-warning text-white hover:bg-warning/80 focus:bg-warning/70',
+    danger: 'bg-danger text-white hover:bg-danger/80 focus:bg-danger/70',
+    light: 'bg-light text-dark hover:bg-light/80 focus:bg-light/70',
+    dark: 'bg-dark text-white hover:bg-dark/80 focus:bg-dark/70'
+};
 
 export const radiusOptions = {
     round: 'rounded-md',
     sharp: 'rounded-none',
     full: 'rounded-full'
-}
+};
 
 export const sizeOptions = {
-    responsive: '',
-    defined: '',
-    full: ''
-}
+    responsive: 'flex-1 flex-shrink-0',
+    normal: 'min-w-24 max-w-28',
+    wide: 'min-w-80 max-w-96',
+    full: 'w-full min-w-full'
+};
 
 export const loadingOptions = {
-    "three-quarter": 'inline-block after:content-[" "] after:block after:size-6 after:border-inherit after:border-b-transparent after:border-[4px] after:rounded-[50%] after:border-solid after:animate-spin'
-}
+    "three-quarter": 'inline-block after:content-[" "] after:block after:size-6 after:border-white after:border-b-transparent after:border-[4px] after:rounded-[50%] after:border-solid after:animate-spin',
+    "three-quarter-dark": 'inline-block after:content-[" "] after:block after:size-6 after:border-dark after:border-b-transparent after:border-[4px] after:rounded-[50%] after:border-solid after:animate-spin'
+};
