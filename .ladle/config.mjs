6abbe518ff2413056964ca62e3e1c61ab5fc87ff
Joseph Young <josephy@dram.space>
2024-02-05 22:23:26 -0600

    feat: Axe-core, base-path

---
+++ .ladle/config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('@ladle/react').UserConfig} */
+export default {
+    base: "/nuui/",
+    outDir: "docs",
+    addons: {
+        a11y: {
+            enabled: true
+        }
+    }
+};
