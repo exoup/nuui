c768431ef7d49857cd0a2caf1d5cc0647c8ff58d
Joseph Young <josephy@dram.space>
2024-02-17 22:45:37 -0600

    Add NonAccordion story

--- src/Content/Accordion.stories.jsx
+++ src/Content/Accordion.stories.jsx
@@ -23,14 +23,12 @@ export default {
 
 export const AccordionComponent = ({ radiusClass, themeClass, flush, ...args }) => (
     <Accordion {...args}
-        initialExpanded={[1]}
         themeClass={themeClass}
         radiusClass={radiusClass}
         className={`w-96`}
-        accordion={false}
         onChange={(isOpen) => console.log('Drawer open status:', isOpen)}
         flush={flush}>
-        <Drawer title="Info" themeClass={themeClass}>
+        <Drawer title="Drawer 1" themeClass={themeClass}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nostrum ab odio aliquid fuga? Ea cumque, iste nulla praesentium quo dolorum esse rem perspiciatis iure. Architecto placeat minima mollitia aspernatur?
         </Drawer>
         <Drawer title="Drawer 2" themeClass={themeClass}>
@@ -43,3 +41,26 @@ export const AccordionComponent = ({ radiusClass, themeClass, flush, ...args }) 
 );
 
 AccordionComponent.storyName = "Accordion";
+
+export const NonAccordionComponent = ({ radiusClass, themeClass, flush, ...args }) => (
+    <Accordion {...args}
+        initialExpanded={[1, 2]}
+        themeClass={themeClass}
+        radiusClass={radiusClass}
+        className={`w-96`}
+        accordion={false}
+        onChange={(isOpen) => console.log('Drawer open status:', isOpen)}
+        flush={flush}>
+        <Drawer title="Drawer 1" themeClass={themeClass}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nostrum ab odio aliquid fuga? Ea cumque, iste nulla praesentium quo dolorum esse rem perspiciatis iure. Architecto placeat minima mollitia aspernatur?
+        </Drawer>
+        <Drawer title="Drawer 2" themeClass={themeClass}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed atque alias, culpa illum officia quia fuga dolore rerum accusantium animi delectus ipsam dignissimos quas, ut officiis asperiores maiores debitis! Harum!
+        </Drawer>
+        <Drawer title="Drawer 3" themeClass={themeClass}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et numquam enim commodi nisi incidunt doloremque totam dolore nobis voluptatem omnis quia odio ex, animi quibusdam facere, nostrum in dignissimos maiores.
+        </Drawer>
+    </Accordion>
+);
+
+NonAccordionComponent.storyName = "OpenableAccordion";
