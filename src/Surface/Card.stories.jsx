cb2b997cc0eb8827dc5cf5f782a4da1b91ae723f
Joseph Young <josephy@dram.space>
2024-02-17 22:44:54 -0600

    Add CardWithAccordion story

--- src/Surface/Card.stories.jsx
+++ src/Surface/Card.stories.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 //Components
 import Card, { Content, Hero, Title, Section } from "./Card";
+import Accordion, { Drawer } from "../Content/Accordion";
 import { Button } from '../Button/Button';
 import { LeadingButton } from "../Button/LeadingButton";
 import { Icon } from "../Icon/Icon";
@@ -27,10 +28,10 @@ export default {
     }
 };
 
-export const CardWithHero = ({ themeClass, textClass, ...args }) => (
+export const CardWithHero = ({ themeClass, ...args }) => (
     <Card {...args} themeClass={themeClass} className="w-80">
         <Hero className="h-64 bg-slate-600 heroPatternClouds" />
-        <Content textClass={themeClass}>
+        <Content themeClass={themeClass}>
             <Title>This is a card component.</Title>
             <p>It uses a Hero with a background-image.</p>
             <p className="mb-4">It also has a full-size Button.</p>
@@ -39,12 +40,12 @@ export const CardWithHero = ({ themeClass, textClass, ...args }) => (
     </Card>
 );
 
-export const CardWithImage = ({ themeClass, textClass, ...args }) => (
+export const CardWithImage = ({ themeClass, ...args }) => (
     <Card {...args} themeClass={themeClass} className="">
         <Hero className={`h-auto`}>
             <img className={`h-auto w-full object-cover`} src="https://source.unsplash.com/user/erondu/700x400" alt="Hero Image" />
         </Hero>
-        <Content textClass={textClass}>
+        <Content themeClass={themeClass}>
             <Title>This is a hero image card.</Title>
             <p className="mb-4">It uses a Hero with an img child and also has a LeadingButton using a div wrapper to position rightly.</p>
             <div className="flex justify-end">
@@ -63,9 +64,9 @@ export const CardWithImage = ({ themeClass, textClass, ...args }) => (
     </Card>
 );
 
-export const CardWithSection = ({ themeClass, textClass, ...args }) => (
+export const CardWithSection = ({ themeClass, ...args }) => (
     <Card {...args} themeClass={themeClass}>
-        <Content textClass={textClass}>
+        <Content themeClass={themeClass}>
             <Title className="text-xl">This is a section card.</Title>
             <Section sections={[
                 { name: "Section 1", element: <p>Section 1</p> },
@@ -76,3 +77,31 @@ export const CardWithSection = ({ themeClass, textClass, ...args }) => (
         </Content>
     </Card>
 );
+
+export const CardWithAccordion = ({ themeClass, radiusClass, flush, ...args }) => (
+    <Card {...args} themeClass={themeClass} radiusClass={radiusClass}>
+        <Hero className="h-64 bg-primary dark:bg-tertiary heroPatternCogs" />
+        <Content themeClass={themeClass}>
+            <Title className="text-xl">This is an accordion card.</Title>
+            <Accordion
+                themeClass={themeClass}
+                flush={flush}>
+                <Drawer title="Drawer 1" themeClass={themeClass}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nostrum ab odio aliquid fuga? Ea cumque, iste nulla praesentium quo dolorum esse rem perspiciatis iure. Architecto placeat minima mollitia aspernatur?
+                </Drawer>
+                <Drawer title="Drawer 2" themeClass={themeClass}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed atque alias, culpa illum officia quia fuga dolore rerum accusantium animi delectus ipsam dignissimos quas, ut officiis asperiores maiores debitis! Harum!
+                </Drawer>
+                <Drawer title="Drawer 3" themeClass={themeClass}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et numquam enim commodi nisi incidunt doloremque totam dolore nobis voluptatem omnis quia odio ex, animi quibusdam facere, nostrum in dignissimos maiores.
+                </Drawer>
+            </Accordion>
+        </Content>
+    </Card>
+);
+
+CardWithAccordion.args = {
+    themeClass: "primary",
+    radiusClass: "round",
+    flush: true
+}
