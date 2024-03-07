a4a6e395160652bd80c5b2951f4f492eae1b7aeb
Joseph Young <josephy@dram.space>
2024-03-06 23:45:47 -0600

    Add avatar to component theme index

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,10 +1,12 @@
 import accordion from "./components/Accordion/Accordion.theme";
+import avatar from "./components/Avatar/Avatar.theme";
 import button from "./components/Button/Button.theme";
 import card from "./components/Card/Card.theme";
 import typography from "./components/Typography/Typography.theme";
 
 const theme = {
     accordion,
+    avatar,
     button,
     card,
     typography
