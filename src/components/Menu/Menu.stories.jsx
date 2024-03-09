4c3e87ddffcb236d81319048cdad8cfa21635331
Joseph Young <josephy@dram.space>
2024-03-08 23:53:02 -0600

    Update Menu story

--- src/components/Menu/Menu.stories.jsx
+++ src/components/Menu/Menu.stories.jsx
@@ -29,15 +29,46 @@ export default {
 };
 
 export const MenuStory = ({ ...args }) => (
+    // <div className="flex justify-between m-2">
+    <Menu>
+        <MenuControl>
+            <Button {...args} className={'flex gap-2 place-content-center'}>
+                Button
+                <ChevronDownIcon className="size-6" />
+            </Button>
+        </MenuControl>
+        <MenuContent {...args} className={'w-44'}>
+            <MenuItem><UserCircleIcon className="size-5" />Profile</MenuItem>
+            <MenuItem><Cog6ToothIcon className="size-5" />Settings</MenuItem>
+            <MenuDivider />
+            <MenuItem disabled><ArrowRightStartOnRectangleIcon className="size-5" />Logout</MenuItem>
+        </MenuContent>
+    </Menu>
+    // </div>
+);
+
+MenuStory.storyName = "Menu"
+
+export const MenuProfile = ({ name, subtitle, ...args }) => (
     <div className="flex justify-between m-2">
         <Menu>
             <MenuControl>
-                <Button {...args} className={'flex gap-2 place-content-center'}>
-                    Button
-                    <ChevronDownIcon className="size-6" />
-                </Button>
+                <AvatarWithText className="cursor-pointer">
+                    <Avatar
+                        {...args}
+                        radius="full"
+                        name={name}
+                    />
+                    <Content typography={args.color}>
+                        <Title>{name}</Title>
+                        <Subtitle>{subtitle}</Subtitle>
+                    </Content>
+                    <ChevronDownIcon className="size-4 dark:text-white" />
+                </AvatarWithText>
             </MenuControl>
-            <MenuContent {...args} className={'w-44'}>
+            <MenuContent
+                {...args}
+                className="w-56">
                 <MenuItem><UserCircleIcon className="size-5" />Profile</MenuItem>
                 <MenuItem><Cog6ToothIcon className="size-5" />Settings</MenuItem>
                 <MenuDivider />
@@ -47,37 +78,8 @@ export const MenuStory = ({ ...args }) => (
     </div>
 );
 
-MenuStory.storyName = "Menu"
-
-// export const MenuProfile = ({ alignment, themeClass, radiusClass, name, subtitle, ...args }) => (
-//     <div className="flex justify-between m-2">
-//         <Menu>
-//             <MenuControl>
-//                 <AvatarWithText className="cursor-pointer">
-//                     <Avatar {...args}
-//                         themeClass={themeClass}
-//                         name={name}
-//                         sizeClass="md"
-//                     />
-//                     <Content themeClass={themeClass}>
-//                         <Title>{name}</Title>
-//                         <Subtitle>{subtitle}</Subtitle>
-//                     </Content>
-//                     <ChevronDownIcon className="size-4 dark:text-white" />
-//                 </AvatarWithText>
-//             </MenuControl>
-//             <MenuContent themeClass={themeClass} radiusClass={radiusClass} alignment={alignment} className="w-56">
-//                 <MenuItem><UserCircleIcon className="size-5" />Profile</MenuItem>
-//                 <MenuItem><Cog6ToothIcon className="size-5" />Settings</MenuItem>
-//                 <MenuDivider />
-//                 <MenuItem disabled={true}><ArrowRightStartOnRectangleIcon className="size-5" />Logout</MenuItem>
-//             </MenuContent>
-//         </Menu>
-//     </div>
-// );
-
-// MenuProfile.storyName = "Profile Menu";
-// MenuProfile.args = {
-//     name: "Just Yogurt",
-//     subtitle: "To brie or not to brie",
-// };
+MenuProfile.storyName = "Profile Menu";
+MenuProfile.args = {
+    name: "Just Yogurt",
+    subtitle: "To brie or not to brie",
+};
