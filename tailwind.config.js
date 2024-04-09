07eadc52e64dab807d59a5460a413a1234c25687
Joseph Young <josephy@dram.space>
2024-04-08 22:43:09 -0500

    Remove

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/*eslint no-undef: "error"*/
-/*eslint-env node*/
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    darkMode: 'class',
-    variants: {},
-    plugins: [require('@tailwindcss/forms'), require('./src/resources/nuui.js')],
-};
